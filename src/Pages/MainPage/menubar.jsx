cfa2dd2f61d6a5307c35fce047b130c3d2aa9409
yebin0310 <yebin060310@gmail.com>
2022-12-30 02:12:41 +0900

    commit

--- src/Pages/MainPage/menubar.jsx
+++ src/Pages/MainPage/menubar.jsx
@@ -1,3 +1,20 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import back from "../../img/뒤로 가기.png";
+const RestartAltIcon = styled.img`
+	position: absolute;
+	right: 300px;
+	bottom: 200px;
+`;
+
 export default function MenuBar({ getCtx, getCanvas }) {
-	return <div></div>;
+	const onReset = () => {
+		getCtx.clearRect(0, 0, getCanvas.width, getCanvas.height);
+	};
+
+	return (
+		<li onClick={onReset}>
+			<RestartAltIcon src={back} className="icons" />
+		</li>
+	);
 }
